9b9ac018d24be7b7f270cd11ffd51898a5f54c77
Jitendra Adhikari <jiten.adhikary@gmail.com>
2019-11-23 07:35:45 +0700

    Add simplified browser bookmarklet snippet

---
+++ browser.js
@@ -0,0 +1,5 @@
+javascript:(_ => {
+  const {redux} = slackDebug.activeTeam;
+  const {wysiwyg_composer, wysiwyg_composer_ios, wysiwyg_composer_webapp, ...payload} = redux.getState().experiments;
+  redux.dispatch({type: '[19] Bulk add experiment assignments to redux', payload});
+})();
